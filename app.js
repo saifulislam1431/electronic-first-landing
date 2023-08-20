831a29598aa4123abfbff0133e1737a372276339
Saiful Islam <sakibbhaiya143@gmail.com>
2023-08-20 21:16:55 +0600

    Search bar added

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const desktop = document.querySelector("#desktop")
 const playstation = document.querySelector("#playstation")
 const modalWrapper = document.querySelector(".modal_wrapper")
 const closeBtn = document.querySelector(".close");
+const searchContainer = document.querySelector(".search-container");
+const searchClose = document.querySelector(".search-close");
+const searchBtn = document.querySelector(".search-btn");
+
 
 nintendo.addEventListener("click",()=>{
 openModal();
@@ -23,6 +27,12 @@ closeBtn.addEventListener("click",()=>{
 modalWrapper.addEventListener("click",(e)=>{
     if(e.target !== modalWrapper) return;
     closeModal();
+})
+searchBtn.addEventListener("click",()=>{
+    searchContainer.classList.add("search-active");
+})
+searchClose.addEventListener("click",()=>{
+    searchContainer.classList.remove("search-active");
 })
 
 const openModal = () =>{
